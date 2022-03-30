70db636c4ed19ace0ebdf20cc962c9960e1ab643
Henriquepython <joaohbp59@gmail.com>
2022-03-30 16:37:45 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  //swagger config
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('The Users API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('docs', app, document);
+  
+  app.enableCors({ origin: '*' });
   await app.listen(3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty()
     email: string;
+    
+    @ApiProperty()
     name: string;
+    
+    @ApiProperty()
     pasword: string;
 }
